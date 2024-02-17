5cdc7d49fc9c151cbc973189ccf5bfcc81e0ef33
mahek <mahekitaliya2005@gmail.cpm>
2024-02-17 09:21:38 +0530

    frist

--- m1.js
+++ m1.js
@@ -9,6 +9,18 @@
 // You must not use any built-in exponent function or operator.
 
 // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
  
 
 
@@ -30,6 +42,23 @@ root();
 // Return the quotient after dividing dividend by divisor.
 
 // Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+// Example 1:
+
+// Input: dividend = 10, divisor = 3
+// Output: 3
+// Explanation: 10/3 = 3.33333.. which is truncated to 3.
+// Example 2:
+
+// Input: dividend = 7, divisor = -3
+// Output: -2
+// Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+ 
+
+// Constraints:
+
+// -231 <= dividend, divisor <= 231 - 1
+// divisor != 0
 
  
 
