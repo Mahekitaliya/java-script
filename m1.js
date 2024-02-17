a65a90fa84dda8350c73411f09b91de50c002999
mahek <mahekitaliya2005@gmail.cpm>
2024-02-17 09:08:23 +0530

    frist

---
+++ m1.js
@@ -0,0 +1,41 @@
+// 69. Sqrt(x)
+// Easy
+// Topics
+// Companies
+// Hint
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+
+
+
+function  root(){
+
+    let n = prompt("enter n:");
+    console.log(Math.sqrt(n));
+}
+root();
+
+// 2
+
+
+// Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+// The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+// Return the quotient after dividing dividend by divisor.
+
+// Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+ 
+
+(function(){
+    let n = prompt("enter n:");
+    let d = prompt("enter divisor:")
+    let j = n / d;
+    console.log(`Divided number is:${parseInt(j)}`);
+})();
