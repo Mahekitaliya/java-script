479f93f94d18f25d6b5541910e51087a7b47d044
mahek <mahekitaliya2005@gmail.cpm>
2024-03-20 08:51:07 +0530

    20-03-2024

---
+++ longestcommne14.js
@@ -0,0 +1,36 @@
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+// **
+//  * @param {string[]} strs
+//  * @return {string}
+//  */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return "";
+    }
+
+    let prefix = strs[0];
+
+    for (let word of strs) {
+        while (word.indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === '') return ''
+        }
+    }
+    return prefix;
+};
